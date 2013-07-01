d69a88de9e1582db0ba7972d902054323b50f6e3
Han <dehanluo@gmail.com>
2013-07-01 16:59:02 -0400

    feat(chapter-02): add solutions for exercises 2.39-2.42
    - Implemented exercise02-39.cc: Demonstration of struct definition and usage

---
+++ chapter-02/exercise02-39.cc
@@ -0,0 +1,21 @@
+
+// Exercise 2.39: Compile the following program to see what happens when you
+// forget the semicolon after a class definition. Remember the message for
+// future reference.
+//     struct Foo { /* empty */ } // Note: no semicolon
+//     int main() {
+//       return 0;
+//     }
+
+struct Foo { /* empty */ }  // Note: no semicolon
+
+int main() {
+  return 0;
+}
+
+// $ g++ -o main exercise02-39.cc && ./main
+// exercise02-39.cc:10:27: error: expected ';' after struct
+//    10 | struct Foo { /* empty */ }  // Note: no semicolon
+//       |                           ^
+//       |                           ;
+// 1 error generated.
