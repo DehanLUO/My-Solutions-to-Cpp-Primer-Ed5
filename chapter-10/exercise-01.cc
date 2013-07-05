0773caab3f3cc85c58cd55c74908a36b034dfb69
Han <dehanluo@gmail.com>
2013-07-05 18:37:26 -0400

    feat(chapter-10): complete exercises for section 10.1
    - Implemented solutions for Chapter 10 exercises:
      * Overview (10.1)

--- chapter-10/exercise-01.cc
+++ chapter-10/exercise-01.cc
@@ -5,9 +5,63 @@
  * count of how many elements have a given value.
  */
 
-int main() { return 0; }
+#include <algorithm>  // std::count
+#include <cstdlib>    // EXIT_SUCCESS
+#include <iostream>   // std::cin, std::cout
+#include <vector>     // std::vector
+
+/*
+ * Counts occurrences of a target value in a vector of integers.
+ * Demonstrates proper usage of STL algorithms with iterator interfaces.
+ */
+size_t CountValueOccurrences(const std::vector<int>& numbers,
+                             int target_value) {
+  // The const qualifier ensures we don't modify the input vector
+  return std::count(numbers.begin(), numbers.end(), target_value);
+}
+
+/*
+ * Reads space-separated integers from standard input into a vector.
+ * Shows robust input handling with clear termination conditions.
+ */
+std::vector<int> ReadIntVectorFromInput() {
+  std::vector<int> result;
+  int current_value;
+
+  /*
+   * Using while-loop with cin extraction operator demonstrates:
+   * 1. Automatic type checking through stream operations
+   * 2. Natural termination on non-integer input
+   */
+  while (std::cin >> current_value) {
+    result.push_back(current_value);
+  }
+
+  // Clear failbit to allow subsequent input operations
+  std::cin.clear();
+  return result;
+}
+
+int main() {
+  std::cout << "Enter target value to count:\n";
+  int target_value;
+  std::cin >> target_value;
+
+  std::cout << "Enter integers separated by spaces (non-integer to stop):\n";
+  const std::vector<int> numbers = ReadIntVectorFromInput();
+
+  const size_t count = CountValueOccurrences(numbers, target_value);
+  std::cout << "The value " << target_value << " appears " << count
+            << " times\n";
+
+  return EXIT_SUCCESS;
+}
 
 /*
  * $ g++ -o main chapter-10/exercise-01.cc && ./main
-
+ * Enter target value to count:
+ * 1
+ * Enter integers separated by spaces (non-integer to stop):
+ * 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ * The value 1 appears 3 times
  */
--- chapter-10/exercise-02.cc
+++ chapter-10/exercise-02.cc
@@ -3,9 +3,67 @@
  * strings.
  */
 
-int main() { return 0; }
+#include <algorithm>  // std::count
+#include <cstdlib>    // EXIT_SUCCESS
+#include <iostream>   // std::cin, std::cout
+#include <list>       // std::list
+#include <string>     // std::string, std::getline
+
+/*
+ * Counts occurrences of a target string in a list of strings.
+ * Demonstrates generic algorithm usage with different container types.
+ */
+size_t CountStringOccurrences(const std::list<std::string>& strings,
+                              const std::string& target) {
+  /*
+   * std::list requires traversal from beginning to end,
+   * but provides better performance for insertions/deletions
+   * compared to vector in middle positions
+   */
+  return std::count(strings.begin(), strings.end(), target);
+}
+
+/*
+ * Reads whitespace-separated strings from standard input into a list.
+ * Shows proper string input handling with getline.
+ */
+std::list<std::string> ReadStringListFromInput() {
+  std::list<std::string> result;
+  std::string current_string;
+
+  /*
+   * Using getline with whitespace delimiter demonstrates:
+   * 1. Proper handling of strings containing spaces
+   * 2. Clear termination on empty line
+   */
+  while (std::getline(std::cin >> std::ws, current_string, ' ')) {
+    if (current_string.empty()) break;
+    result.push_back(current_string);
+  }
+
+  return result;
+}
+
+int main() {
+  std::cout << "Enter target string to count:\n";
+  std::string target;
+  std::getline(std::cin >> std::ws, target);
+
+  std::cout << "Enter strings separated by spaces (empty line to stop):\n";
+  const std::list<std::string> strings = ReadStringListFromInput();
+
+  const size_t count = CountStringOccurrences(strings, target);
+  std::cout << "The string \"" << target << "\" appears " << count
+            << " times\n";
+
+  return EXIT_SUCCESS;
+}
 
 /*
  * $ g++ -o main chapter-10/exercise-02.cc && ./main
-
+ * Enter target string to count:
+ * the
+ * Enter strings separated by spaces (empty line to stop):
+ * The quick brown fox jumps over the lazy dog.
+ * The string "the" appears 1 times
  */
